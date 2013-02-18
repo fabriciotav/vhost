7d744de6650c55d20baee6e2d9183af9ccb23ece
Fabricio Tavares de Oliveira <fabriciotavareso@gmail.com>
2013-02-18 14:27:43 -0300

    Update to add celula.in subdomains
    Also add valeriatavares.com domain.

--- vhost.js
+++ vhost.js
@@ -12,9 +12,25 @@ var app,
 express    = require('express');
 
 // ..........................................................
-// APP :: api.celula.in
+// APP :: bhjs.in
+bhjs = express();
+appBhjs = require('../bhjs/server').boot(bhjs);
+
+// ..........................................................
+// APP :: celula.in
 //
 celula = express();
+
+// ..........................................................
+// APP :: api.celula.in
+//
+apiCelula = express();
+routesCelula = require('../celula-rest-api/routes').routes(celula);
+
+// ..........................................................
+// APP :: dev.celula.in
+//
+devCelula = express();
 routesCelula = require('../celula-rest-api/routes').routes(celula);
 
 // ..........................................................
@@ -32,18 +48,21 @@ sitio = express();
 routesSitio = require('../Sitio/settings').routes(sitio);
 
 // ..........................................................
-// APP :: bhjs.in
-bhjs = express();
+// APP :: valeriatavares.com
+valeria = express();
 
 // ..........................................................
 // MAIN APP
 app = express();
 
 app.use(express.vhost('bhjs.in', bhjs));
-app.use(express.vhost('api.celula.in', celula));
+app.use(express.vhost('celula.in', celula));
+app.use(express.vhost('api.celula.in', apiCelula));
+app.use(express.vhost('dev.celula.in', devCelula));
 app.use(express.vhost('dadospublicos.org', dp));
 app.use(express.vhost('sitiodochicao.com.br', sitio));
 app.use(express.vhost('um-a-um.com', um));
+app.use(express.vhost('valeriatavares.com', valeria));
 
 app.listen(80);
 console.log( "Server started at port 80" );
